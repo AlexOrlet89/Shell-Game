b4920cf81d5bcb6c062f85feb5d3bb156c161072
Alex Orlet <AlexOrlet89@gmail.com>
2022-01-24 17:50:11 -0800

    added button listener and ballLocation, validated randomness

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ let wins = 0;
 let total = 0;
 // set event listeners 
 
+button1.addEventListener('click', () => {
+// console.log('clicked');
+//reset reveal class in order to add reveal later.
+
+// ball3.classList.remove('reveal');
+// ball1.classList.remove('reveal');
+// ball2.classList.remove('reveal');
+
+total++;
+const ballLocation = Math.ceil(Math.random() * 3);
+console.log('ball is in location', ballLocation);
+
+
+});
+
+button2.addEventListener('click', () => {
+console.log('clicked');
+});
+
+button3.addEventListener('click', () => {
+console.log('clicked');
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
