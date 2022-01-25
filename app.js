3606680ff15e77965d3d4059e2ef98ebed583b38
Alex Orlet <AlexOrlet89@gmail.com>
2022-01-24 16:55:43 -0800

    finished HTML, grabbed dom elements and set let statements

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 // import functions and grab DOM elements
+const ball1 = document.getElementById('ball1');
+const ball2 = document.getElementById('ball2');
+const ball3 = document.getElementById('ball3');
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+const winSpan = document.getElementById('wins');
+const lossSpan = document.getElementById('Losses');
+const totalSpan = document.getElementById('Total');
 
 // let state
 
+let wins = 0;
+let total = 0;
 // set event listeners 
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
